95b8e63e0bc9400530bcd8584703e15ad1d81b77
Wagner da Silva Junior <wagnerjr306@gmail.com>
2026-08-09 17:03:26 +0000

    instalei o breeze e criei a migration

---
+++ app/Http/Controllers/TransfersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transfers;
+use Illuminate\Http\Request;
+
+class TransfersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(transfers $transfers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(transfers $transfers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, transfers $transfers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(transfers $transfers)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_08_09_162524_create_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // chave estrangeira para a tabela users e o constrained() cria a relação automaticamente e o onDelete('cascade') garante que, se um usuário for deletado, todas as suas vendas também serão deletadas
+            $table->string('order_code', 100);
+            $table->date('sale_date');
+            $table->decimal('gross_amount', 8, 2);
+            $table->decimal('commission_amount', 8, 2);
+            $table->decimal('fee_amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
